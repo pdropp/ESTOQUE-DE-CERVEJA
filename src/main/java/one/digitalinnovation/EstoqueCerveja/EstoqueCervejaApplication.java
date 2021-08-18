65ee251ec8e5d0ba52c4cc199f9b560689d8a52a
Pedro Paulo <61791649+pdropp@users.noreply.github.com>
2021-08-18 20:49:57 -0300

    Pdropp

---
+++ src/main/java/one/digitalinnovation/EstoqueCerveja/EstoqueCervejaApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.EstoqueCerveja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueCervejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueCervejaApplication.class, args);
+	}
+
+}
